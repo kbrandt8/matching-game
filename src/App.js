1e848e8ffecad31d83fe1b0b26e4bbcf8094eb41
Kaitlyn <kmbrandt08@hotmail.com>
2021-12-04 20:10:16 -0600

    First Commit!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, {useContext} from "react"
+import {Context} from "./Context"
+import Matches from "./Matches"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  
+const {allItems,matched,add,startOver,turns,gameData} = useContext(Context)
+
+  const items = allItems.map(item=>
+  (<Matches key={item.id} item={item} onClick={()=>add(item)}/>))
+  
+  return(
+  <div>   
+      <h1>Matching Game</h1>
+      <h1>Score: {matched.length}/6 Turns : {turns}</h1>
+      <h2 onClick={()=>startOver()}>{gameData}</h2>
+  <div className="container">
+    
+ {items}
+    
+ </div>
+        
+  </div>
+    )
 }
 
 export default App;
