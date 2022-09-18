cb1c3a4ba0c3e0fc4dfb58d2cd64eb194995b87e
Kaitlyn <kmbrandt08@hotmail.com>
2022-09-18 00:38:29 -0500

    start over yet again

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ app.post("/addScore", async (req,res)=>{
 })
 
   
-// if (process.env.NODE_ENV === 'production') {}
+ if (process.env.NODE_ENV === 'production') {
  app.use(express.static(path.join('client/build')));
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-  
+}
 
 
 
